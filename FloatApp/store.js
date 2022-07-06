9d2fcc9994d3fa9a5d327e192d6f1e119af9cbe8
Aditya Manapure <73328745+Aditya-Manapure@users.noreply.github.com>
2022-07-06 16:16:35 +0530

    Delete FloatApp directory

--- FloatApp/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import navReducer from "./src/slices/navSlice";
-
-export const store = configureStore({
-    reducer : {
-        nav : navReducer,
-    },
-});
